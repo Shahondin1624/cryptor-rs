aa1b88b43c9727330409ff8a13de2fe57774f20e
Shahondin1624 <shahondin1624@gmail.com>
2023-03-12 14:28:38 +0100

    Beautified cli

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 #![feature(iter_collect_into)]
 
+use std::env::consts::OS;
 use std::fs;
 use std::path::Path;
 use std::time::{Duration, Instant};
 
 use atomic_counter::{AtomicCounter, RelaxedCounter};
 use clap::Parser;
+use colorful::{Color, Colorful};
+use colorful::Color::Green;
+use colorful::core::color_string::CString;
 use env_logger::Builder;
+use indicatif::{ParallelProgressIterator, ProgressBar, ProgressStyle};
 use log::{debug, error, info, warn};
 use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 use walkdir::{DirEntry, WalkDir};
@@ -21,20 +26,26 @@ mod encryption;
 
 fn main() {
     let args = CryptorArguments::parse();
-    println!("Type in the password you want to use for the operation");
+    print_colorful_when_supported("Type in the password you want to use for the operation", |msg| msg.color(Green));
     let password = rpassword::read_password().unwrap();
-    println!("Repeat the password to ensure there are no typos");
+    print_colorful_when_supported("Repeat the password to ensure there are no typos", |msg| msg.color(Green));
     let password_check = rpassword::read_password().unwrap();
     if !password.eq(&password_check) {
         panic!("The passwords did not match!");
     }
-    println!("root_file: {}\nencryption_mode: {}", args.root_file_path, args.mode);
+    let argument_string = format!("root_file: {}\nencryption_mode: {}", args.root_file_path, args.mode);
+    let argument_string = argument_string.as_str();
+    print_colorful_when_supported(argument_string, |msg| msg.gradient(Color::Red));
     let args = CryptorArgumentsWithPassword::from(args, password);
     Builder::new().filter_level(args.verbose.log_level_filter()).init();
     let mode = args.mode.clone();
     match iterate_over_file_tree(args) {
         None => { error!("Could not perform {}-operation", mode) }
-        Some(duration) => { info!("Whole {}-operation took {:?}", mode, duration) }
+        Some(duration) => {
+            let message = format!("Whole {}-operation took {:?}", mode, duration);
+            let message = message.as_str();
+            print_colorful_when_supported(message, |msg| msg.gradient(Color::Green));
+        }
     }
 }
 
@@ -67,7 +78,8 @@ fn perform_operation_on_files(args: CryptorArgumentsWithPassword, operable_files
     info!("Beginning encryption...");
     let argon2_config = encryption::argon2_config();
     let counter = RelaxedCounter::new(0);
-    operable_files.par_iter().for_each(|entry| {
+    let bar = create_progress_bar(operable_files.len() as u64);
+    operable_files.par_iter().progress_with(bar).for_each(|entry| {
         let path = entry.path().to_string_lossy().to_string();
         debug!("{}/{} files already encrypted", counter.get(), operable_files.len());
         match args.mode {
@@ -97,4 +109,36 @@ fn can_manipulate(path: &Path) -> bool {
         Err(_) => { return false; }
     };
     !permissions.readonly()
+}
+
+pub fn is_colorful_supported() -> bool {
+    "linux".eq(OS)
+}
+
+fn print_colorful_when_supported(message: &str, function: fn(&str) -> CString) {
+    if is_colorful_supported() {
+        let message = function(message);
+        println!("{}", message);
+    } else {
+        println!("{}", message);
+    }
+}
+
+fn create_progress_bar(items: u64) -> ProgressBar {
+    let bar = ProgressBar::new(items);
+    bar.enable_steady_tick(Duration::from_secs(1));
+    let style: Option<ProgressStyle> = match ProgressStyle::with_template
+        ("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}") {
+        Ok(_style) => { Some(_style) }
+        Err(_) => {
+            debug!("Could not retrieve progress bar style!");
+            None
+        }
+    };
+    if style.is_some() {
+        let mut style = style.unwrap();
+        style = style.progress_chars("##-");
+        bar.set_style(style);
+    }
+    bar
 }
